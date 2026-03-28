017d1d216b18759c31a3d5f38f8a8e4412bfe1b4
DenisVargaeu <mail@denisvarga.eu>
2026-03-28 10:22:36 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+function S_HOW_HEARTH () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+}
+basic.forever(function () {
+	
+})
